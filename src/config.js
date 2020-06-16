08709dc0a4400950d56ed085c1508d9f5c8b0ccb
kattisrik <srikanthkatti.katti@gmail.com>
2020-06-16 13:51:38 +0530

    Setting up things

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "notes-app-uploads-katti"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://opt61iwzzi.execute-api.ap-south-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_LBinifpEU",
+      APP_CLIENT_ID: "50jh61akmuert83ltu4f18t0i2",
+      IDENTITY_POOL_ID: "ap-south-1:d96663ed-c627-4c5c-ba97-8ca1ab070292"
+    }
+  };
