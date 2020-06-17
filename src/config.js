45f6b8a4259a3c26e45163cf591a95d8e3250a76
kattisrik <srikanthkatti.katti@gmail.com>
2020-06-17 17:34:19 +0530

    final changes made

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51GuI8UDyCRFuyK4FkN7NpD7G9yonb5kGv9Q22iG0Bez5EWIc67uNjLubnWGq19lqC3PaUylwiJiJxf5FOTXolJMv00OgvzbNjP",
     s3: {
       REGION: "ap-south-1",
       BUCKET: "notes-app-uploads-katti"
