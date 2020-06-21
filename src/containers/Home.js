0f51a27ab067fc79297309f861312bb569b6920a
kattisrik <srikanthkatti.katti@gmail.com>
2020-06-22 00:02:14 +0530

    Updating home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
+import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
 import "./Home.css";
 
@@ -60,6 +61,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
